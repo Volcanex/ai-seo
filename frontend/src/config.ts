cdd21f29371e9ce7428ae8c5b9630b37d48cf017
Volcanex <gabrielpenman@gmail.com>
2024-10-29 18:53:16 +0000

    untrack the bastard

--- frontend/src/config.ts
+++
@@ -1,5 +0,0 @@
-// src/config.ts
-
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || ''
-// export const API_URL = process.env.NEXT_PUBLIC_API_URL || '/api'
-// This used to be set to /api, really all the calls in the tsx files should not be `${API_URL}/api/models/${modelId}` and should in fact be `${API_URL}/models/${modelId}`, this current setup is dumb, please fix gabe
